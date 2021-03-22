ca25e11d2a4e0229f99c0766365e48db11c5a657
unknown <edpxavier@gmail.com>
2021-03-22 15:30:49 -0300

    Finalizando tela de produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -101,6 +101,34 @@ class _ProductScreenState extends State<ProductScreen> {
                       );
                     }).toList(),
                   )),
+              SizedBox(
+                height: 16.0,
+              ),
+              SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                    onPressed: size != null ? () {} : null,
+                    color: primaryColor,
+                    textColor: Colors.white,
+                    child: Text("Adicionar ao Carrinho",
+                        style: TextStyle(fontSize: 18.0)),
+                  )),
+              SizedBox(
+                height: 16.0,
+              ),
+              Text(
+                "Descrição",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                product.description,
+                style: TextStyle(
+                  fontSize: 16.0,                  
+                ),
+              ),
             ],
           ),
         )
